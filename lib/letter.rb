c5249e789b08d3e0c1a1983da2f724bac23f64ca
minsi yang <yangminsi@gmail.com>
2020-07-13 21:36:23 +0100

    refactored get_middle method

--- lib/letter.rb
+++ lib/letter.rb
@@ -1,7 +1,5 @@
 def get_middle(word)
-  if word.length <= 2
-    return word
-  elsif word.length.odd?
+  if word.length.odd?
     return word[word.length/2]
   else
     return word[word.length/2 - 1] + word[word.length/2]
