35d51c3a3e6b11fb016e5a1ae05bd2467f721b14
minsi yang <yangminsi@gmail.com>
2020-07-13 21:38:48 +0100

    refactoring inplicit 'return'

--- lib/letter.rb
+++ lib/letter.rb
@@ -2,9 +2,9 @@ def get_middle(word)
   i = word.length/2
 
   if word.length.odd?
-    return word[i]
+    word[i]
   else
-    return word[i - 1] + word[i]
+    word[i - 1] + word[i]
   end
-  
+
 end
